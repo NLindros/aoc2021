df782873de4b691351654a69bfca761aa8bb9d82
Nils Lindros <nils.lindros@niradynamics.se>
2021-12-01 11:39:21 +0100

    Simplified sliding window
    Change-Id: Ib15deffd2e2ee235b2e2d6a2fb15120cc9dec0fb

--- day01/aoc.py
+++ day01/aoc.py
@@ -3,37 +3,36 @@ from pathlib import Path
 from itertools import pairwise, islice
 from collections import deque
 
+
 def read_input(file_path=Path(__file__).parent / "input.txt"):
     with open(file_path, "r") as fid:
         return [int(val) for val in fid]
 
-def depth_increases(depths):
+
+def increases_of_depth(depths):
     return sum((b > a) for a, b in pairwise(depths))
 
-def sliding_window(iterable, win_size):
-    it = iter(iterable)
-    window = deque(islice(it, win_size), maxlen=win_size)
-    if len(window) == win_size:
+
+def sliding_window(values, win_size):
+    window = deque(islice(values, win_size), maxlen=win_size)
+    for x in values:
         yield tuple(window)
-    for x in it:
         window.append(x)
-        yield tuple(window)
+    yield tuple(window)
 
 
-def sliding_mean_increases(dephts):
-    sums = (sum(win) for win in sliding_window(dephts, win_size=3))
-    return depth_increases(sums)
+def increases_of_depth_with_sliding_mean(dephts):
+    win_sums = (sum(win) for win in sliding_window(dephts, win_size=3))
+    return increases_of_depth(win_sums)
 
 
 if __name__ == "__main__":
-
     part = os.environ.get("part")
     solver = {
-        "part1": depth_increases,
-        "part2": sliding_mean_increases,
+        "part1": increases_of_depth,
+        "part2": increases_of_depth_with_sliding_mean,
     }
 
     file_input = read_input("input.txt")
     result = solver[part](file_input)
-
     print(result)
--- day01/aoc_test.py
+++ day01/aoc_test.py
@@ -1,28 +1,28 @@
 import unittest
 import aoc
 
+
 class TestInput(unittest.TestCase):
     def test_read_input(self):
         result = aoc.read_input()
         self.assertTrue(all(isinstance(val, int) for val in result))
 
-class TestPart1(unittest.TestCase):
 
+class TestPart1(unittest.TestCase):
     def test_example(self):
         depth = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-        self.assertEqual(aoc.depth_increases(depth), 7)
+        self.assertEqual(aoc.increases_of_depth(depth), 7)
 
     def test_real_input(self):
         depth = aoc.read_input()
-        self.assertEqual(aoc.depth_increases(depth), 1266)
+        self.assertEqual(aoc.increases_of_depth(depth), 1266)
 
 
 class TestPart2(unittest.TestCase):
-
     def test_example(self):
         depth = [607, 618, 618, 617, 647, 716, 769, 792]
-        self.assertEqual(aoc.sliding_mean_increases(depth), 5)
+        self.assertEqual(aoc.increases_of_depth_with_sliding_mean(depth), 5)
 
     def test_real_input(self):
         depth = aoc.read_input()
-        self.assertEqual(aoc.sliding_mean_increases(depth), 1217)
+        self.assertEqual(aoc.increases_of_depth_with_sliding_mean(depth), 1217)
