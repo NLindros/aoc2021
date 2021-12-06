9124d14626ab0b872edc3c6a255be2a5f9af0275
Nils Lindros <nils.lindros@niradynamics.se>
2021-12-06 13:32:46 +0100

    Scrap some lines, make more unreadable
    Change-Id: I33777ec43b24a35bee54cc188e54a5754650eb8a

--- day06/aoc.py
+++ day06/aoc.py
@@ -1,15 +1,12 @@
 import os
-from collections import deque, Counter
+import collections
 
 with open("input.txt", "r") as fid:
-    ages = map(int, fid.readline().split(","))
+    amount = collections.Counter(map(int, fid.readline().split(",")))
 
-days = {"part1": 80, "part2": 256}[os.environ.get("part")]
+population_map = collections.deque(amount[idx] for idx in range(9))
 
-amount = Counter(ages)
-population_map = deque(amount[idx] for idx in range(9))
-
-for day in range(days):
+for day in range({"part1": 80, "part2": 256}[os.environ.get("part")]):
     population_map.rotate(-1)
     population_map[6] += population_map[8]
 
