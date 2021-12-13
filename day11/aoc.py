f3f2d4bc4ac2dc2eda26f6d0482f22074fac31a6
Nils Lindros <nils.lindros@niradynamics.se>
2021-12-13 10:23:10 +0100

    Day11
    Change-Id: I6dd691e4c79c53058084b5f4aa4a473e21a62736

--- day11/aoc.py
+++ day11/aoc.py
@@ -1,10 +1,7 @@
-from itertools import chain
 import os
+from itertools import chain
 from math import prod, inf
 from typing import List
-
-
-MAX = 9
 
 
 def read_input(file_path="input.txt"):
@@ -13,86 +10,16 @@ def read_input(file_path="input.txt"):
 
 
 def solve_part1(data):
-    is_min_point = is_smallest_in_neighborhood(data)
-    return sum(
-        [
-            val + 1
-            for line, cond_line in zip(data, is_min_point)
-            for val, cond in zip(line, cond_line)
-            if cond
-        ]
-    )
 
-
-def is_smallest_in_neighborhood(matrix):
-    height, width = len(matrix), len(matrix[0])
-    stream = list(chain(*matrix))
-    horizontal = min_point_in_stream(stream, width=width, height=height)
-    transpose_data = list(chain(*zip(*matrix)))
-    vertical = min_point_in_stream(transpose_data, width=height, height=width)
-    return [h & v for v, h in zip(horizontal, vertical)]
-
-
-def min_point_in_stream(stream, width, height):
-    left = [True, *[a < b for a, b in zip(stream[1:], stream[:-1])]]
-    left[slice(None, None, width)] = [True] * height
-    right = [*[a > b for a, b in zip(stream[1:], stream[:-1])], True]
-    right[slice(width - 1, None, width)] = [True] * height
-    return [l & r for l, r in zip(left, right)]
-
-
-def left_to_right(line):
-    left = (True, *[a > b for a, b in zip(line[:-1], line[1:])])
-    right = (*[a < b for a, b in zip(line[:-1], line[1:])], True)
-    is_min_point = (l & r for l, r in zip(left, right))
-    return is_min_point
-
-
-def get_basins(data):
-    is_min_point = is_smallest_in_neighborhood(data)
-    min_points = [
-        (i, j)
-        for (i, row), cond_line in zip(enumerate(data), is_min_point)
-        for (j, val), cond in zip(enumerate(row), cond_line)
-        if cond
-    ]
-    groups = [get_group_around_point(data, point) for point in min_points]
-    group_size = sorted(map(len, groups))
-    return prod(group_size[-3:])
-
-
-def get_group_around_point(data, start):
+    flashes = 0
     height, width = len(data), len(data[0])
-    start_idx = width * start[0] + start[1]
-    group = [start_idx]
-    stream = list(chain(*data))
-    last_row = width * (height - 1)
-    for idx in group:
-        for new_idx in get_adjacent_points(width, last_row, idx):
-            if new_idx not in group:
-                if stream[new_idx] < MAX:
-                    group.append(new_idx)
-    return group
 
+    data = [x + 1 for x in line for line in data]
 
-def get_adjacent_points(width, last_row, idx):
-    adjacent = filter(
-        None,
-        [
-            idx - 1 if idx % width else None,
-            idx + 1 if (idx + 1) % width else None,
-            idx - width if idx >= width else None,
-            idx + width if idx < last_row else None,
-        ],
-    )
-
-    return adjacent
-
-
-def basin_points(data: List[list]) -> List[tuple]:
-    return [
-        (i, j) for i, row in enumerate(data) for j, val in enumerate(row) if val < MAX
-    ]
+    for i, line in enumerate(data):
+        for j, x in enumerate(line):
+            if x > 9:
+                flashes += 1
 
 
 if __name__ == "__main__":
--- day11/test_aoc.py
+++ day11/test_aoc.py
@@ -14,63 +14,18 @@ class TestInput(unittest.TestCase):
 
 class TestPart1(unittest.TestCase):
     def test_small_example(self):
-        data = [[2, 3, 5, 4], [3, 1, 4, 5]]
-        result = aoc.solve_part1(data)
-        self.assertEqual(result, 10)
-
-    def test_part1_example(self):
-        data = aoc.read_input(curr_dir / "example_input.txt")
-        result = aoc.solve_part1(data)
-        self.assertEqual(result, 15)
-
-    def test_compare_neighbors(self):
         data = [
-            [1, 2, 1],
-            [4, 3, 2],
-            [4, 1, 2],
+            [1, 9, 9, 9, 1],
+            [1, 9, 1, 9, 1],
+            [1, 9, 9, 9, 1],
+            [1, 1, 1, 1, 1],
         ]
-
-        self.assertEqual(
-            aoc.is_smallest_in_neighborhood(data),
-            [[True, False, True], [False, False, False], [False, True, False]],
-        )
-
-    def test_part1_with_input(self):
-        t0 = time()
-        data = aoc.read_input(curr_dir / "input.txt")
-        result = aoc.solve_part1(data)
-        print(time() - t0)
-        self.assertEqual(result, 580)
-
-    def test_min_points(self):
-        data = [3, 4, 2, 3, 2, 1, 4, 3]
-        min_point = aoc.min_point_in_stream(data, width=4, height=2)
-        self.assertEqual(
-            min_point, [True, False, True, False, False, True, False, True]
-        )
-
-
-class TestPart2(unittest.TestCase):
-    def test_part2_example(self):
-        data = aoc.read_input(curr_dir / "example_input.txt")
-        result = aoc.get_basins(data)
-        self.assertEqual(result, 1134)
-
-    def test_collect_adjacent_from_point(self):
-        data = [
-            [9, 9, 1, 2],
-            [5, 4, 9, 9],
-            [3, 2, 5, 9],
-            [9, 6, 9, 8],
-            [9, 9, 9, 7],
+        expected = [
+            [3, 4, 5, 4, 3],
+            [4, 0, 0, 0, 4],
+            [5, 0, 0, 0, 5],
+            [4, 0, 0, 0, 4],
+            [3, 4, 5, 4, 3],
         ]
-        result = aoc.get_group_around_point(data, start=(2, 1))
-        self.assertEqual(len(result), 6)
-
-    def test_part2_with_input(self):
-        t0 = time()
-        data = aoc.read_input(curr_dir / "input.txt")
-        result = aoc.get_basins(data)
-        t1 = time()
-        print(t1 - t0)
-        self.assertEqual(result, 856716)
+        result = aoc.solve_part1()
+        self.assertEqual(result,expected)
